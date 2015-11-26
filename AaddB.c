472723281fee21a60decc3f5d77e776112892395
Little Choppe <piiy@163.com>
2015-11-26 20:06:57 +0800

    add
    A add B

---
+++ AaddB.c
@@ -0,0 +1,25 @@
+/*
+2015年11月26日19:31:34
+目的：复习C，求两个整数之和
+*/
+
+# include <stdio.h>
+
+int main()
+{
+    int a, b, sum = 0;
+    a = 123;
+    b = 456;
+    sum = a + b;
+    printf("sum is %d.\n", sum);
+
+    return 0;
+}
+
+/*
+输出：
+sum is 579.
+
+Process returned 0 (0x0)   execution time : 0.166 s
+Press any key to continue.
+*/
