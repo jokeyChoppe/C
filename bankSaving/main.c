8048fd298b7318ff715583762e78e0bc3b91a730
Little Choppe <piiy@163.com>
2015-11-30 15:21:48 +0800

    review c
    chapter 3 exercise

---
+++ bankSaving/main.c
@@ -0,0 +1,34 @@
+/*
+2015年11月30日10:43:57
+目的：计算银行存款
+*/
+
+# include <stdio.h>
+# include <math.h>
+
+int main()
+{
+    int years;
+    float p, r1, r2, r3, r5, r0;
+    freopen("./bankSaving.txt", "r", stdin);
+    scanf("%f%d%f%f%f%f%f", &p, &years, &r1, &r2, &r3, &r5, &r0);
+    printf("方案一的本息和为%f\n", p * (1 + years * r5 / 100));
+    printf("方案二的本息和为%f\n", p * (1 + 2 * r2 / 100)*(1 + 3 * r3 / 100));
+    printf("方案三的本息和为%f\n",  p * (1 + 3 * r3 / 100)*(1 + 2 * r2 / 100));
+    printf("方案四的本息和为%f\n", p * pow(1+r1/100, 5));
+    printf("方案五的本息和为%f\n", p * pow(1+r0/4/100, 4*years));
+
+    return 0;
+}
+
+/*
+输出：
+方案一的本息和为1292.499995
+方案二的本息和为1270.763199
+方案三的本息和为1270.763199
+方案四的本息和为1224.863982
+方案五的本息和为1036.622301
+
+Process returned 0 (0x0)   execution time : 0.078 s
+Press any key to continue.
+*/
